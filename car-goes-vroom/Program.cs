29dc564624108c86c42c252c25d7b11cbb714890
Peter van der Voorn <peteranthonius@gmail.com>
2021-08-04 14:20:43 +1200

    initial commit

---
+++ car-goes-vroom/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace car_goes_vroom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
